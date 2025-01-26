b5508d281e21809592bd08c46329d6993404f4c5
shravani264 <2024bit030@sggs.ac.in>
2025-01-26 13:22:42 +0530

    Add files via upload

---
+++ basic code.c
@@ -0,0 +1,34 @@
+
+//To find perimwter of rectangle
+/*#include<stdio.h>
+int main() {
+    float L, B, perimeter;
+
+    // Input the length and width of the rectangle
+    printf("Enter the length of the rectangle: ");
+    scanf("%f", &L);
+    
+    printf("Enter the width of the rectangle: ");
+    scanf("%f", &B);
+
+    // Calculate the perimeter
+    perimeter = 2 * (L + B);
+
+    // Display the result
+    printf("The perimeter of the rectangle is: %f\n", perimeter);
+
+return 0;
+}*/
+#include<stdio.h>
+int main(){
+
+   int n;
+   int cube = n*n*n;
+   printf("Enter the value of n:");
+   scanf ("%d",&n);
+   //display the result;
+    printf("The cubic value of given number is:%d",cube);
+
+
+ return 0;   
+}
